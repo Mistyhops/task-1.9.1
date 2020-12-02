a361bbcfd5f806a4504393cdb26087796b3bf837
Mistyhops <nedgalkin@gmail.com>
2020-12-02 18:26:56 +0300

    Taks 1.9.1

---
+++ rectangle0.py
@@ -0,0 +1,22 @@
+class Circle:
+    def __init__(self, a):
+        self.a = a
+
+    def getCircleSquare(self):
+        return self.a ** 2 * 3,14159
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ rectangle_import.py
@@ -0,0 +1,13 @@
+from python_practice import rectangle0
+from rectangle0 import Circle
+
+
+print('Радиус площади круга:')
+
+circle_1 = Circle(4)
+circle_2 = Circle(10)
+
+print(circle_1.getCircleSquare())
+
+
+
