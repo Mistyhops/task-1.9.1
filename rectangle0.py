d5ee7bd8644ce5a4f93cd239be27cbe2e0778792
Mistyhops <nedgalkin@gmail.com>
2020-12-03 18:56:40 +0300

    Исправлено

--- rectangle0.py
+++ rectangle0.py
@@ -1,9 +1,22 @@
+class Rectangle:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def getArea(self):
+        return self.a * self.b
+
+class Square:
+    def __init__(self, a):
+        self.a = a
+    def squareArea(self):
+        return self.a ** 2
+
 class Circle:
     def __init__(self, a):
         self.a = a
 
     def getCircleSquare(self):
-        return self.a ** 2 * 3,14159
+        return self.a ** 2 * 3.14159
 
 
 
--- rectangle_import.py
+++ rectangle_import.py
@@ -1,13 +1,37 @@
-from python_practice import rectangle0
-from rectangle0 import Circle
+from completed_tasks.rectangle0 import Circle, Rectangle, Square
 
+rect_1 = Rectangle(3,4)
+rect_2 = Rectangle(12,5)
 
-print('Радиус площади круга:')
+print('Площадь прямоугольника:')
+print(rect_1.getArea())
+print(rect_2.getArea())
+
+square_1 = Square(5)
+square_2 = Square(10)
+
+print('Площадь квадрата:')
+print(square_1.squareArea(),
+      square_2.squareArea())
 
 circle_1 = Circle(4)
 circle_2 = Circle(10)
 
+print('Площадь круга:')
 print(circle_1.getCircleSquare())
 
 
+print('-'*15)
+figures = [rect_1, rect_2, square_1, square_2, circle_1, circle_2]
+
+for figure in figures:
+    if isinstance(figure, Square):
+        print('Площадь квадрата:')
+        print(figure.squareArea())
+    elif isinstance(figure, Circle):
+        print('Площадь круга:')
+        print(figure.getCircleSquare())
+    else:
+        print('Площадь прямоугольника:')
+        print(figure.getArea())
 
